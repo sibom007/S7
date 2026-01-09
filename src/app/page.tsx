04ecb3c80ee0293a6bc49e5462d415496db831bf
sibom007 <vaispark400@gmail.com>
2026-01-09 23:39:45 +0600

    01.Database  added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
 "use client";
 import { ThemeToggle } from "@/components/theme-toggle";
 import { Button } from "@/components/ui/button";
+import { useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
+import { Authenticated, Unauthenticated } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
+  const tasks = useQuery(api.tasks.get);
   return (
-    <div>
-      <Button>hello</Button>
+    <div className="">
       <ThemeToggle />
+
+      <Authenticated>
+        <Button>hello</Button>
+        <UserButton />
+        {tasks?.map(({ _id, text }) => (
+          <div key={_id}>{text}</div>
+        ))}
+      </Authenticated>
+      <Unauthenticated>
+        <SignInButton />
+      </Unauthenticated>
     </div>
   );
 }
